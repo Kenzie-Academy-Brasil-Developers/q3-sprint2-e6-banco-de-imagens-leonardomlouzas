f6a353b5af50dd05465eb7da7a7e76ddb898d715
Leonardo <leonardomlouzas@gmail.com>
2022-03-01 22:21:09 -0300

    Feat: finish /files and /files/<> endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,60 @@
-# Desenvolva suas rotas aqui
+from flask import Flask, request
+import os
+from .kenzie.image import all_images, selected_images
+
+app = Flask(__name__)
+
+
+def initial_config():
+
+    FILES_DIRECTORY=os.getenv("FILES_DIRECTORY")
+    ALLOWED_EXTENSIONS=os.getenv("ALLOWED_EXTENSIONS")
+
+    directories = os.listdir("./")
+
+    if FILES_DIRECTORY not in directories:
+        os.mkdir(FILES_DIRECTORY)
+        os.chdir(FILES_DIRECTORY)
+        os.system(f"mkdir {ALLOWED_EXTENSIONS}")
+    else:
+        directories = os.listdir(FILES_DIRECTORY)
+
+        if not "jpg" in directories:
+            os.mkdir(f"{FILES_DIRECTORY}/jpg")
+        
+        if not "png" in directories:
+            os.mkdir(f"{FILES_DIRECTORY}/png")
+        
+        if not "gif" in directories:
+            os.mkdir(f"{FILES_DIRECTORY}/gif")
+        
+
+initial_config()
+
+@app.get("/")
+def home():
+    return "home"
+
+@app.get("/files")
+def files():
+    return all_images()
+
+@app.get("/files/<extension>")
+def files2(extension):
+    return selected_images(extension)
+
+@app.post("/upload")
+def upload():
+    data = request.get_json()
+    return (f"upload: {data}")
+
+@app.get("/download/<file_name>")
+def download1(file_name):
+    return(f"download1: {file_name}")
+
+@app.get("/download-zip")
+def download2():
+    file_extension = request.args.get("file_extension")
+    compression_ratio = request.args.get("compression_ratio")
+
+    return(f"file_extension: {file_extension}. compression_ratio: {compression_ratio}")
--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,1 +1,44 @@
-# Desenvolva sua lógica de manipulação das imagens aqui
+from flask import jsonify
+import os
+
+ALLOWED_EXTENSIONS = os.getenv("ALLOWED_EXTENSIONS")
+
+def all_images():
+    result = []
+
+    tmp_list = []
+
+    list_items = os.walk("./images")
+
+    for item in list_items:
+        for file in item[2]:
+            tmp_list.append({"Name": file})
+        
+        if len(tmp_list) > 0:
+            result.append({f'{item[0]}': tmp_list})
+        tmp_list = []
+
+    if len(result) > 0:
+        return jsonify(result), 200
+    
+    return {"msg":"No files"}, 404
+
+def selected_images(type):
+    if type in ALLOWED_EXTENSIONS:
+        
+        result = []
+
+        list_items = os.walk(f"./images/{type}")
+
+        for item in list_items:
+            for file in item[2]:
+                result.append({"Name": file})
+
+        if len(result) > 0:
+
+            return jsonify(result), 200
+    
+    return {"msg":f"No '{type}' files"}, 404
+
+
+#TODO: CREATE UPLOAD/DOWNLOAD FUNCTIONS
